ee9949d0523c70571ca24e32a298ef998df6c924
PRAKASH K <>
2024-04-22 14:40:49 +0530

    dev: service mapping form

---
+++ src/components/shared/DivyangDetails.js
@@ -0,0 +1,74 @@
+import { Box, Grid, Typography, styled } from "@mui/material";
+
+import user from "../../assets/profile.png";
+import { WithCondition } from "./WithCondition";
+
+const StyledImage = styled("img")({
+  width: 175,
+  height: 200,
+  objectFit: "cover",
+});
+
+const DetailSection = styled(Box)({
+  display: "flex",
+});
+
+const Title = styled(Typography)({
+  width: 180,
+  display: "flex",
+  justifyContent: "space-between",
+  marginRight: 16,
+  fontWeight: 700,
+});
+
+const defaultDivyangDetailColumns = [
+  {
+    accessor: "name",
+    Header: "Name",
+  },
+  {
+    accessor: "mobileNo",
+    Header: "Mobile No",
+  },
+  {
+    accessor: "divyangId",
+    Header: "Divyang Id",
+  },
+  {
+    accessor: "aadharNo",
+    Header: "Aadhar No",
+  },
+];
+
+const DivyangDetail = ({
+  divyangDetail,
+  disableProfile = false,
+  divyangDetailColumns = [],
+}) => {
+  return (
+    <>
+      <WithCondition isValid={!disableProfile}>
+        <Grid item xs={4}>
+          <StyledImage
+            src={divyangDetail?.profileImageUrl || user}
+            onError={(e) => (e.target.src = user)}
+            alt="Profile picture"
+          />
+        </Grid>
+      </WithCondition>
+
+      <Grid item xs={8}>
+        {defaultDivyangDetailColumns?.map((item, key) => (
+          <WithCondition isValid={!!divyangDetail[item?.accessor]}>
+            <DetailSection key={key + item?.accessor} sx={{ display: "flex" }}>
+              <Title>{`${item?.Header} : `}</Title>
+              <Typography>{divyangDetail[item?.accessor]}</Typography>
+            </DetailSection>
+          </WithCondition>
+        ))}
+      </Grid>
+    </>
+  );
+};
+
+export default DivyangDetail;
--- src/constants/serviceMapping/serviceMapping.js
+++ src/constants/serviceMapping/serviceMapping.js
@@ -42,3 +42,75 @@ export const listFields = {
     size: "small",
   },
 };
+
+export const formFields = {
+  searchDivyangId: {
+    label: "Search By Divyang ID",
+    name: "searchDivyangId",
+  },
+  searchMobileNo: {
+    label: "Search By Mobile No",
+    name: "searchMobileNo",
+  },
+  searchAadharNo: {
+    label: "Search By Aadhar No",
+    name: "searchAadharNo",
+  },
+  searchUDIDNo: {
+    label: "Search By UDID No",
+    name: "searchUDIDNo",
+  },
+  state: {
+    label: "Select seva kendra state",
+    name: "state",
+  },
+  district: {
+    label: "Select seva kendra district",
+    name: "district",
+  },
+  sevaKendra: {
+    label: "Select seva kendra",
+    name: "sevaKendra",
+  },
+  assignUser: {
+    label: "Assign user",
+    name: "assignUser",
+  },
+  serviceType: {
+    label: "Select service type",
+    name: "serviceType",
+  },
+  serviceSubtype: {
+    label: "Select service sub type",
+    name: "serviceSubtype",
+  },
+  service: {
+    label: "Select service",
+    name: "service",
+  },
+  dateOfService: {
+    label: "Date of service",
+    name: "dateOfService",
+  },
+  completedBefore: {
+    label: "Service to be completed before",
+    name: "completedBefore",
+  },
+  contactPersonName: {
+    label: "Contact Person name",
+    name: "contactPersonName",
+  },
+  mobileNo: {
+    label: "Mobile No",
+    name: "mobileNo",
+  },
+  emailId: {
+    label: "Email",
+    name: "emailId",
+  },
+  emailStatus: {
+    label: "Mail divyang details",
+    name: "emailStatus",
+    inputValues: [],
+  },
+};
