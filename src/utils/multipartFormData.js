e21ee254afb66c367050c489e815c56c53667be8
premwnl <>
2024-04-25 12:47:46 +0530

    Fix : multipart form data changed to nested objects

--- src/utils/multipartFormData.js
+++ src/utils/multipartFormData.js
@@ -1,22 +1,19 @@
 export const multiPartFormData = (workData, nullables = []) => {
-  let form = new FormData();
+  const form = new FormData();
 
   const appendFormData = (data, parentKey = "") => {
-    Object.keys(data).forEach((key) => {
-      const value = data[key];
+    Object.entries(data).forEach(([key, value]) => {
       const newKey = parentKey ? `${parentKey}[${key}]` : key;
 
       if (typeof value === "object" && value !== null) {
         appendFormData(value, newKey);
-      } else {
-        if (value !== "" || nullables.includes(newKey)) {
-          form.append(
-            newKey,
-            typeof value === "string"
-              ? value.trim().replace(/\s{2,}/g, " ")
-              : value
-          );
-        }
+      } else if (value !== "" || nullables.includes(newKey)) {
+        form.append(
+          newKey,
+          typeof value === "string"
+            ? value.trim().replace(/\s{2,}/g, " ")
+            : value
+        );
       }
     });
   };
