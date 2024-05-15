1ca513e21490eea2eb461a21c0a9688d2b7ed78e
premwnl <>
2024-05-15 20:07:38 +0530

    dev : dev service mapping

--- src/styles/common.js
+++ src/styles/common.js
@@ -1,10 +1,10 @@
 import { Typography, styled } from "@mui/material";
 
 export const CustomTypography = styled(Typography)(
-  ({ theme, capitalize, color }) => ({
+  ({ theme, capitalize, color, fontSize }) => ({
     color: color || theme?.palette?.commonColor?.blue,
     textTransform: capitalize === "capitalize" ? "capitalize" : "uppercase",
-    fontSize: "16px",
+    fontSize: fontSize || "16px",
     fontWeight: "bold",
     marginBottom: 16,
     userSelect: "none",
